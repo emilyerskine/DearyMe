b2247d31c8f0835cdf0b2e60ca508369598328c5
emily erskine <2575176@dundee.ac.uk>
2025-11-25 23:11:06 +0000

    add symptoms dropdown

--- app.js
+++ app.js
@@ -5,9 +5,15 @@ if ("serviceWorker" in navigator) {
   .catch(error => console.log("Service Worker Registration Failed", error));
 }
 
+//for entries
 const entryInput = document.getElementById("entry");
 const saveBtn = document.getElementById("saveBtn");
 const entriesList = document.getElementById("entriesList");
+//for symptoms
+const symptoms = ["Stress", "Anxiety", "Low Mood", "Fatigue",]
+const symptomSelected = [];
+const display = document.querySelector("#symptomsMulti .multi-display");
+const options = document.querySelector("#symptomsMulti .multi-options");
 
 // track mood
 let selectedMood = null;
@@ -25,6 +31,7 @@ saveBtn.addEventListener("click", () => {
     text,
     timestamp: new Date().toLocaleString(),
     mood: selectedMood || "No Mood Logged",
+    symptoms: [...symptomSelected] || "No Symptoms Logged",
   };
 
   entries.unshift(newEntry);
@@ -38,7 +45,7 @@ function renderEntries() {
   entriesList.innerHTML = "";
   entries.forEach((e) => {
     const li = document.createElement("li");
-    li.textContent = `${e.timestamp} — (${e.mood}) — ${e.text}`;
+    li.textContent = `${e.timestamp} — ${e.mood} — [${e.symptoms.join(", ")}] — ${e.text} `;
     entriesList.appendChild(li);
   });
 }
@@ -57,3 +64,27 @@ function updateMoodUI() {
   });
 }
 
+//add symptoms drop down
+display.addEventListener("click", () => options.classList.toggle("hidden"));
+
+symptoms.forEach(sym => {
+  const div = document.createElement("div");
+  div.textContent = sym;
+  div.addEventListener("click", () => toggleSymptom(sym));
+  options.appendChild(div);
+});
+
+function toggleSymptom(sym) {
+  if (symptomSelected.includes(sym)) {
+    symptomSelected.splice(symptomSelected.indexOf(sym), 1);
+  } else {
+    symptomSelected.push(sym);
+  }
+  updateSymptomsUI();
+}
+
+function updateSymptomsUI() {
+  display.textContent  = symptomSelected.length
+  ? symptomSelected.join(", ")
+  : "Select symptoms";
+}
