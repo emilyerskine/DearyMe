d1524b8b13134fdd4d2a54a8f68c63e30f387a1a
emily erskine <2575176@dundee.ac.uk>
2025-11-25 21:54:24 +0000

    Add mood select function

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 // Register service worker
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("service-worker.js");
+  navigator.serviceWorker.register("service-worker.js")
+  .then(() => console.log("Service Worker Registered"))
+  .catch(error => console.log("Service Worker Registration Failed", error));
 }
 
 const entryInput = document.getElementById("entry");
 const saveBtn = document.getElementById("saveBtn");
 const entriesList = document.getElementById("entriesList");
 
-// Load existing entries
+// track mood
+let selectedMood = null;
+
+// load existing entries
 let entries = JSON.parse(localStorage.getItem("diaryEntries")) || [];
 renderEntries();
 
+//save entries
 saveBtn.addEventListener("click", () => {
   const text = entryInput.value.trim();
   if (!text) return;
@@ -18,6 +24,7 @@ saveBtn.addEventListener("click", () => {
   const newEntry = {
     text,
     timestamp: new Date().toLocaleString(),
+    mood: selectedMood || "No Mood Logged",
   };
 
   entries.unshift(newEntry);
@@ -26,11 +33,27 @@ saveBtn.addEventListener("click", () => {
   renderEntries();
 });
 
+//add entries
 function renderEntries() {
   entriesList.innerHTML = "";
   entries.forEach((e) => {
     const li = document.createElement("li");
-    li.textContent = `${e.timestamp} — ${e.text}`;
+    li.textContent = `${e.timestamp} — (${e.mood}) — ${e.text}`;
     entriesList.appendChild(li);
   });
 }
+
+//add mood selection buttons 
+document.querySelectorAll("#moodSelector button").forEach(btn => {
+  btn.addEventListener("click", () => {
+    selectedMood = btn.dataset.value;
+    updateMoodUI();
+  });
+});
+
+function updateMoodUI() {
+  document.querySelectorAll("#moodSelector button").forEach(btn => {
+    btn.classList.toggle("selected", btn.dataset.value === selectedMood);
+  });
+}
+
